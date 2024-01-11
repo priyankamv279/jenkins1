bc2670ae3eebfd6ca3960b14852e674ca91b1b36
PRIYANKA M V <134724191+priyankamv279@users.noreply.github.com>
2024-01-11 12:46:43 +0530

    now

---
+++ springawsdemo/Dockerfile
@@ -0,0 +1,4 @@
+From openjdk:8
+EXPOSE 8081
+ADD target/springawsdemo-0.0.1-SNAPSHOT.war  springawsdemo-0.0.1-SNAPSHOT.war
+ENTRYPOINT [ "java","-jar","/springawsdemo-0.0.1-SNAPSHOT.war" ]
---
+++ springawsdemo/src/main/java/com/example/demo/SpringawsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringawsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringawsdemoApplication.class, args);
+	}
+
+}
